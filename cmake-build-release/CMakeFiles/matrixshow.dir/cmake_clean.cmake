2d0c238f0481d4ba9701ea1dc33ef9493f187e63
MagnavoxTG <dominik.lohner92@gmx.at>
2019-11-24 17:59:17 +0100

    Release 1.0

---
+++ cmake-build-release/CMakeFiles/matrixshow.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/matrixshow.dir/main.c.o"
+  "matrixshow"
+  "matrixshow.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/matrixshow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
